dddb085b193add49bcd6a6ee3a95f39d933f526f
Abdulazeez8954 <143010631+Abdulazeez8954@users.noreply.github.com>
2026-08-30 01:08:59 +0100

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,8 @@
-// Corridor Funding - Firebase Configuration
-const firebaseConfig = {
-    apiKey: "AIzaSyDmFXgouE7sN2sDiHOgnX-p0BauxLxCepk",
-    authDomain: "corridorfunding-62580.firebaseapp.com",
-    projectId: "corridorfunding-62580",
-    storageBucket: "corridorfunding-62580.firebasestorage.app",
-    messagingSenderId: "750873524882",
-    appId: "1:750873524882:web:02c406f158c225a728444b"
+export const firebaseConfig = {
+  apiKey: "AIzaSyDmFXgouE7sN2sDiHOgnX-p0BauxLxCepk",
+  authDomain: "corridorfunding-62580.firebaseapp.com",
+  projectId: "corridorfunding-62580",
+  storageBucket: "corridorfunding-62580.firebasestorage.app",
+  messagingSenderId: "750873524882",
+  appId: "1:750873524882:web:02c406f158c225a728444b"
 };
-
-export default firebaseConfig;
